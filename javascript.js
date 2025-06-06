0cd67872af55eeac3881de1bc040d09d90907684
FleyLT <luaeliseu@gmail.com>
2025-06-05 22:45:57 -0300

    Feito trabalho

---
+++ javascript.js
@@ -0,0 +1,4 @@
+  const botao = document.getElementById('meuBotao');
+        botao.addEventListener('click', function() {
+        alert('✨ "Tecnologia é o meio. Conectar, criar e evoluir é o propósito."');
+        });
